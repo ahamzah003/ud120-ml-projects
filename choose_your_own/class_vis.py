357f67a0f066d1420dd4037eb1d453f707efd6f6
Abdulrahman Hamzat <abdulrahmanhamzat3@gmail.com>
2025-04-24 18:00:59 +0100

    switched prettyPicture axis labels

--- choose_your_own/class_vis.py
+++ choose_your_own/class_vis.py
@@ -30,8 +30,8 @@ def prettyPicture(clf, X_test, y_test):
     plt.scatter(grade_sig, bumpy_sig, color = "b", label="fast")
     plt.scatter(grade_bkg, bumpy_bkg, color = "r", label="slow")
     plt.legend()
-    plt.xlabel("bumpiness")
-    plt.ylabel("grade")
+    plt.xlabel("grade")
+    plt.ylabel("bumpiness")
 
     plt.savefig("test.png")
 
