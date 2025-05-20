02568835df679fb35240155bf13ee7878d5c3ebb
Abdulrahman Hamzat <abdulrahmanhamzat3@gmail.com>
2025-05-20 12:50:09 +0100

    fit regression on test set

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -69,6 +69,8 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
