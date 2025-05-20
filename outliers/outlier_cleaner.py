852606fd39e711b48cc3e45e0dac4d54c0025698
Abdulrahman Hamzat <abdulrahmanhamzat3@gmail.com>
2025-05-20 17:37:14 +0100

    fill in cleaning algorithm

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -13,8 +13,12 @@ def outlierCleaner(predictions, ages, net_worths):
     
     cleaned_data = []
 
-    ### your code goes here
-
-    
+    # create a list of tuples (age, net_worth, error)
+    error = [(age, net_worth, abs(prediction - net_worth)) for prediction, age, net_worth in zip(predictions, ages, net_worths)]
+    # sort list in ascending order by absolute error
+    error_sorted = sorted(error, key=lambda x: x[2])
+    # remove the 10% of points with the largest errors
+    limit = int(len(error_sorted) * 0.9)
+    cleaned_data = error_sorted[:limit]
     return cleaned_data
 
